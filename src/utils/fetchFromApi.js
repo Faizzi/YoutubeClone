f0d956493802d66e1df86273e627ce009b24d7b4
Faizan Ullah <93101478+Faizzi@users.noreply.github.com>
2023-07-20 11:00:32 +0500

    In Youtube Clone App, Navbar, SideBar created and Videos fetched from API

---
+++ src/utils/fetchFromApi.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+const API_KEY ="0f242f93b9mshe5710e2b2b8c9bbp123ea3jsn9d18c228b30f"
+const options = {
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": API_KEY,
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromApi = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
